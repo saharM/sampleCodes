8fdf2f7bad0f14818d3bab76bfa5a0ff78b53a5a
Sahar Mohammadi <sahar.mohammadi@persianswitch.com>
2014-04-06 16:04:39 +0330

    added "C# Socket Programming"

---
+++ CSharp Socket Programming/Time Client/Time Client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Time_Client
+{
+    class Program
+    {
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            LoopConnect();
+            SendLoop();
+            Console.ReadLine();
+        }
+
+        private static void SendLoop()
+        {
+            while (true)
+            {
+                //Thread.Sleep(500);
+                //byte[] buffer = Encoding.ASCII.GetBytes("get time");
+                Console.Write("Enter a request: ");
+                string req = Console.ReadLine();
+                byte[] buffer = Encoding.ASCII.GetBytes(req);
+                _clientSocket.Send(buffer);
+
+                byte[] receivedBuf = new byte[1024];
+                int rec = _clientSocket.Receive(receivedBuf);
+                byte[] data = new byte[rec];
+                Array.Copy(receivedBuf, data, rec);
+                Console.WriteLine("Received: " + Encoding.ASCII.GetString(data));
+            }
+        }
+
+        private static void LoopConnect()
+        {
+            int attempts = 0;
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect(IPAddress.Loopback, 100);
+                }
+                catch(SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Connection attempts: " + attempts.ToString());
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+    }
+}
